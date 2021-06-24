76cc46496240ba27b30c3e63c60e8d7bc5a1fa5f
John Cabang <johncabz@gmail.com>
2021-06-24 09:03:31 -0400

    Handling Functions - Typing

--- src/App.tsx
+++ src/App.tsx
@@ -25,19 +25,19 @@ function App() {
   const [people, setPeople] = useState<IState["people"]>([
     {
       name: "Tesla",
-      url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tesla.com%2Fmodelx&psig=AOvVaw18utofXvStO61lWvMKH-zl&ust=1624624325567000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCODGwrKjsPECFQAAAAAdAAAAABAD",
+      url: "https://www.topgear.com/sites/default/files/images/cars-road-test/carousel/2019/08/e91c11ad3914389003b4316d6a6823b0/dsc01999.jpg?w=1784&h=1004",
       age: 18,
       note: "We will not stop until every car on the road is electric",
     },
     {
       name: "SpaceX",
-      url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nasa.gov%2Fpress-release%2Fas-artemis-moves-forward-nasa-picks-spacex-to-land-next-americans-on-moon&psig=AOvVaw3QF66c4c3glaQRhYQlLQ5Q&ust=1624624436600000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIDOjfCjsPECFQAAAAAdAAAAABAD",
+      url: "https://www.nasa.gov/sites/default/files/thumbnails/image/for_press_release.jpg",
       age: 19,
       note: "I always have optimism, but I'm realistic. It was not with the expectation of great success that I started Tesla or SpaceX... It's just that I thought they were important enough to do anyway",
     },
     {
       name: "Nasa",
-      url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nasa.gov%2Fpress-release%2Fcrew-1-astronauts-safely-splash-down-after-space-station-mission&psig=AOvVaw01qzJKk1eT1T1cS_8dbnnK&ust=1624624501560000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCMig8ImksPECFQAAAAAdAAAAABAD",
+      url: "https://www.nasa.gov/sites/default/files/thumbnails/image/nhq202105020001.jpg",
       age: 63,
       note: "That's one small step for man, one giant leap for mankind",
     },
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,13 +12,22 @@ interface IProps {
 // DEFINE List as a REACT FUNCTIONAL COMPONENT THAT CONTAINS TYPE OF IProps
 
 const List: React.FC<IProps> = ({ people }) => {
-  return (
-    <>
-      {people.map((person) => {
-        return <>{person.name}</>;
-      })}
-    </>
-  );
+  const renderList = (): JSX.Element[] => {
+    return people.map((person) => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.url} alt="avatar" />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
 };
 
 export default List;
